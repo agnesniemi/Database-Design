04b2d9fc5908528befadb611478b3b8d7efb5f88
agni0310 <agnes.niemi.0310@student.uu.se>
2023-12-08 00:21:02 +0100

    Fixing the possibility for user to ask for department, leaf department seems to work

--- Milestone4_test.py
+++ Milestone4_test.py
@@ -1,14 +1,30 @@
-import pymysql
 
+
+import pymysql
 mydb = pymysql.connect(
     host='127.0.0.1',
-    user='ht23_1_group_48',
+    user='ht23_2_group_48',
     password='pasSWd_48'
 )
 
 mycursor = mydb.cursor ()
 
 mycursor. execute ("SHOW DATABASES")
+
+for x in mycursor:
+    print (x)
+
+mycursor. execute ("USE ht23_2_project_group_48")
+mycursor. execute ("SELECT * FROM Department")
+
+for x in mycursor:
+    print (x)
+
+
+dep_id = input('Choose Department ID: ')
+
+# IF LEAF DEPARTMENT
+mycursor. execute (f"SELECT Product_title, (Price_before_tax + Tax_added_to_price + Sale) AS Current_Retail_Price FROM Product WHERE Product_title IN (SELECT Product_title FROM Product WHERE Department_title = '{dep_id}')")
 
 for x in mycursor:
     print (x)
