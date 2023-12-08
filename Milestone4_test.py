69d89da7720c70c4e29c7b408bd6e8333b188b04
agni0310 <agnes.niemi.0310@student.uu.se>
2023-12-08 08:09:38 +0100

    Added a template for further work (ie departments that are not leaf and products)

--- Milestone4_test.py
+++ Milestone4_test.py
@@ -1,26 +1,31 @@
 
 
 import pymysql
+
+# CONNECTING SO SQL CAN BE USED
 mydb = pymysql.connect(
     host='127.0.0.1',
     user='ht23_2_group_48',
     password='pasSWd_48'
 )
 
+# WILL INTERPRET YOUR SQL-COMMAND AND GIVE AN ITERABLE WITH THE OBTAINED OBJECTS
 mycursor = mydb.cursor ()
 
 mycursor. execute ("SHOW DATABASES")
 
-for x in mycursor:
-    print (x)
-
-mycursor. execute ("USE ht23_2_project_group_48")
-mycursor. execute ("SELECT * FROM Department")
+# THESE SECTIONS ARE FOR DISPLAYING THE OBTAINED OBJECTS GENERATED FROM A SQL COMMAND
 
 for x in mycursor:
     print (x)
 
+mycursor. execute ('USE ht23_2_project_group_48')
+mycursor. execute ('SELECT * FROM Department')
 
+for x in mycursor:
+    print (x)
+
+# USER CHOOSES DERARTMENT ID (PRIMARY KEY TITLE)
 dep_id = input('Choose Department ID: ')
 
 # IF LEAF DEPARTMENT
@@ -28,6 +33,27 @@ mycursor. execute (f"SELECT Product_title, (Price_before_tax + Tax_added_to_pric
 
 for x in mycursor:
     print (x)
+
+# IF NOT LEAF
+'''
+mycursor. execute (f'SQL COMMAND')
+
+
+for x in mycursor:
+    print (x)
+'''
+
+# USER CHOOSES PRODUCT ID
+prod_id = input('Choose Product ID: ')
+'''
+mycursor. execute (f'SQL COMMAND')
+
+
+for x in mycursor:
+    print (x)
+'''
+
+
 
 mydb.close()
 
